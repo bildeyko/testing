6643376cca76169e852d5f9b0e5078e3564f29da
Nikolay Bildeyko <bildeyko@gmail.com>
2016-05-29 13:54:33 +0300

    Rewrite in webdriver

--- lab3/src/com/bildeyko/Browser.java
+++ lab3/src/com/bildeyko/Browser.java
@@ -1,13 +1,19 @@
 package com.bildeyko;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by ASUS on 29.05.2016.
  */
 public class Browser {
-    public static String get() {
-        String browser = "*firefox C:\\Program Files (x86)\\Firefox Developer Edition\\firefox.exe";
-        //String browser = "*googlechrome";
-        
-        return browser;
+    public static WebDriver get() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+        //WebDriver driver = new ChromeDriver();
+
+        return driver;
     }
 }
--- lab3/tests/SearchTest.java
+++ lab3/tests/SearchTest.java
@@ -1,34 +1,55 @@
 import com.thoughtworks.selenium.*;
+import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import com.bildeyko.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import static org.junit.Assert.*;
 import java.util.regex.Pattern;
 
 public class SearchTest {
-	private Selenium selenium;
+	private WebDriver driver;
+	private String baseUrl;
 
 	@Before
 	public void setUp() throws Exception {
-		String browser = Browser.get();
-
-		selenium = new DefaultSelenium("localhost", 4444, browser, "http://www.ivi.ru/");
-		selenium.start();
+		driver = Browser.get();
+		baseUrl = "http://www.ivi.ru/";
 	}
+
+	/*@Test
+	public void test1() throws Exception {
+		driver.get(baseUrl);
+		selenium.open("/");
+		selenium.type("//header/div[3]/form/div/input", "Нолан");
+		selenium("//header/div[3]/form/div/input", "Нолан");
+		selenium.click("//ul[@class='persons-result']//a[contains(text(),'Кристофер Нолан')]");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Кристофер Нолан", selenium.getText("//h1"));
+	}*/
 
 	@Test
-	public void testSearch() throws Exception {
-		selenium.open("/");
-		selenium.type("//header/div[3]/form/div/input", "Начало");
-		selenium.typeKeys("//header/div[3]/form/div/input", "Начало");
-		selenium.click("//header/div[3]/form/div/button");
-		selenium.waitForPageToLoad("30000");
-		assertEquals("Начало", selenium.getText("//section[@class='gallery-wrapper ']/ul/li[2]//span[@class='title']/span"));
+	public void test3() throws Exception {
+		driver.get(baseUrl);
+		WebElement element;
+		element = driver.findElement(By.xpath("//header/div[3]/form/div/input"));
+		element.sendKeys("Начало");
+
+		element = driver.findElement(By.xpath("//header/div[3]/form/div/button"));
+		element.click();
+		//element.sendKeys(Keys.ENTER);
+
+		WebDriverWait wait = new WebDriverWait(driver,5);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//section[@class='gallery-wrapper ']/ul/li[2]//span[@class='title']/span")));
+		element = driver.findElement(By.xpath("//section[@class='gallery-wrapper ']/ul/li[2]//span[@class='title']/span"));
+		assertEquals("Начало", element.getText());
 	}
 
-	@After
-	public void tearDown() throws Exception {
-		selenium.stop();
-	}
 }
